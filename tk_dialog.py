d47ca1259f250aa2a469f47c7d77f1763063ca43
mfmain <mfmain@sina.com>
2021-09-13 16:20:34 +0800

    a

---
+++ tk_dialog.py
@@ -0,0 +1,54 @@
+
+import tkinter as tk
+
+
+class TkDialog(tk.Toplevel):
+    def __init__(self, master, *la, **ka):
+        super().__init__(master, *la, **ka)
+        self.transient(master)
+        self.grab_set()
+        self.master = master
+
+        #self.geometry("400x300")
+        x,y = master.winfo_pointerxy()
+        # self.geometry("+%d+%d"%(x-200,y-100))
+        self.geometry("+%d+%d" % (x, y))
+
+        self.result = None
+
+    def close(self, result):
+        self.result = result
+        self.destroy()
+
+    def run(self):
+        self.wait_window(self)
+        return self.result
+
+
+if __name__ == '__main__':
+    class DlgDemo(TkDialog):
+        def __init__(self, master, *la, **ka):
+            super().__init__(master, *la, **ka)
+
+            self.uiv_entry = tk.IntVar(value=1)
+            tk.Entry(self, width=4, textvariable=self.uiv_entry, justify="center").pack(fill="both", expand=0)
+
+            fr = tk.Frame(self)
+            fr.pack(side="top", pady=5, fill="both")
+            tk.Button(fr, text="确认", command=self.on_确认).pack(side="left", fill="both", expand=1)
+            tk.Button(fr, text="取消", command=self.on_取消).pack(side="left", fill="both", expand=1)
+
+        def on_确认(self):
+            self.destroy()
+            self.result = self.uiv_entry.get()
+
+        def on_取消(self):
+            self.destroy()
+            self.result = None
+
+    root = tk.Tk()
+    dlg = DlgDemo(root)
+    result = dlg.run()
+    print(result)
+
+
